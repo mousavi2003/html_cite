3812d341dcf76c66612e24b9be289bfc5c07a9ba
Fatemeh <me.mousavi1382@gmail.com>
2021-12-10 16:13:47 +0330

    commiting from pc

---
+++ java.js
@@ -0,0 +1,13 @@
+﻿<script src="jquery-3.4.0.min.js"></script>
+
+function confirmed(){
+    var check = confirm("آیا حذف این پیام را تایید می کنید؟");
+    if (check == 1){
+        document.getElementById('text').innerHTML = '';
+        alert("پیام حذف شد");
+    }
+    else{
+        document.getElementById('text').innerHTML = 'در صورت تایید، این پیام حذف خواهد شد';
+        alert("حذف پیام لغو شد");
+    }
+}
